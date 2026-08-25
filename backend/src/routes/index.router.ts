df91837ff64b65697189026699e6bb03f2c7d41d
Adithya karmarkar <adithyakg31@gmail.com>
2026-08-25 14:44:05 +0530

    webhooks configuration done correctly

--- backend/src/routes/index.router.ts
+++ backend/src/routes/index.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { authLimiter } from "@middleware/rateLimiter.middleware";
+import { authLimiter, webhookLimiter } from "@middleware/rateLimiter.middleware";
 import authRouter from './auth.router';
 import webHookRouter from './webhook.router'
 const router=Router();
 
-router.use('/v1',authLimiter,authRouter)
-router.use('/v1',webHookRouter)
+router.use('/v1/auth',authLimiter,authRouter)
+router.use('/v1/webhooks',webhookLimiter,webHookRouter)
 
 export default router
