568746e02f3139ec130797efa956cdc1f0d2cc38
Adithya karmarkar <adithyakg31@gmail.com>
2026-09-01 14:35:00 +0530

    standar path

--- backend/src/webHooks/handlePushEvent.ts
+++ backend/src/webHooks/handlePushEvent.ts
@@ -3,7 +3,7 @@ export const handlePushEvent=async(payload:any)=>{
 try{
   const installationId = payload.installation.id;
   const repository=payload.repository;
-  const repoPath=await getOrCreateRepoPath(installationId,repository.id);
+  const repoPath=await getOrCreateRepoPath(installationId,repository);
   console.log('repoPath:',repoPath)
   return repoPath;
 
