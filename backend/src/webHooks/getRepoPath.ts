30f8703798fdf2651c2594ccebc9b24582ff0e1b
Adithya karmarkar <adithyakg31@gmail.com>
2026-09-03 18:58:08 +0530

    just dumy

--- backend/src/webHooks/getRepoPath.ts
+++ backend/src/webHooks/getRepoPath.ts
@@ -40,7 +40,9 @@ export const getOrCreateRepoPath = async (
     fs.mkdirSync(repoPath, { recursive: true });
     const GITHUB_TOKEN = await getInstallationId(installationId);
     console.log(GITHUB_TOKEN);
-    await simpleGit(repoPath).clone(`https://x-access-token:${GITHUB_TOKEN}@github.com/${repo.name}.git`);
+    await simpleGit(repoPath).clone(
+  `https://x-access-token:${GITHUB_TOKEN}@github.com/${repo.full_name}.git`
+);
     return repoPath;
   } catch (error) {
     throw error;
